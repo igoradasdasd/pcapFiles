39bbf08994eed9dd8e7923a42e3c4b587c65ca2d
Axte <axte@yandex.ru>
2025-01-23 18:31:00 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
  *  Created on: 23 янв. 2025 г.
  *      Author: Игорь
  */
+#include <string>
 
 
+int main()
+{
+	// считывание данных
+	std::string sourceFile1("./files/1.pcap");
+	std::string sourceFile2("./files/2.pcap");
+
+}
 
 
