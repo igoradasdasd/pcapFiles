151f33d1a80535a387dbcce79d47a1a5c59b7095
Axte <axte@yandex.ru>
2025-01-25 09:36:30 +0300

    Почистил

--- main.cpp
+++ main.cpp
@@ -16,44 +16,13 @@
 
 int main()
 {
-	// считывание данных
+	// входные данные
 	std::string sourceFile1("./pcapFiles/1.pcap");
 	std::string sourceFile2("./pcapFiles/2.pcap");
 	std::string resultFile("./pcapFiles/result.pcap");
 
 	std::shared_ptr<Merge> merge( new Merge(sourceFile1, sourceFile2, resultFile ) );
 	merge->work();
-
-
-/*
-	std::ifstream f11;
-
-	std::ofstream resultF1;
-
-	f11.open(sourceFile1, std::ios::binary);
-	resultF1.open(resultFile, std::ios::binary);
-
-	char ff;
-	for (int i = 0; i < 24; ++i)
-	{
-		f11.read((char*)&ff, sizeof(ff));
-		resultF1.write((char*)&ff, sizeof(ff));
-	}
-*/
-//	merge->work();
-/*
-	f11.read((char*)&ff, sizeof(ff));
-	resultF1.write((char*)&ff, sizeof(ff));
-
-	/*
-	std::ofstream f;
-	f.open("./pcapFiles/1.txt");
-	f.seekp(24, std::ios::cur);
-	f.close();
-
-//	fseek(f, 24*8, SEEK_CUR);
-*/
-
 }
 
 
